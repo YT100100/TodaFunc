af79fc178b5884dac85ce3c4811e94d78f549a52
Yusuke Toda <toda@ut-biomet.org>
2020-08-27 11:16:34 +0900

    Expanded head.mat and tail.mat to list or vector object

--- R/head.mat.R
+++ R/head.mat.R
@@ -1,5 +1,20 @@
 head.mat <- function (mat, n.row = 6, n.col = 6) {
+
+  # If list is given, first element will be chosen
+  while (class(mat) == 'list') {
+    warning("List object was given. Only the first element was shown.")
+    mat <- mat[[1]]
+  }
+
+  # if vector is given, use common head() function
+  if (is.null(dim(mat))) {
+    return(head(mat))
+  }
+
+  # show the first 6 columns and rows
   disp.row <- pmin(nrow(mat), n.row)
   disp.col <- pmin(ncol(mat), n.col)
-  mat[1:disp.row, 1:disp.col]
+  ans <- mat[1:disp.row, 1:disp.col]
+  return(ans)
+
 }
