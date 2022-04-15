78f07b50b9c1cb9fbcee4e078a668ceac22f7c5a
Yusuke Toda <toda@ut-biomet.org>
2022-04-15 13:29:35 +0900

    Create exe_new_proc_pattern()
    解析を回すときに使うスクリプト
    以前の結果を読み込み、まだ行っていないパターンの解析を回すことができる
    一通りのデバッグは行ったが、場合によってはまだおかしな挙動があるかもしれない

---
+++ R/exe_new_proc_pattern.R
@@ -0,0 +1,176 @@
+remove_dup_row <- function(base_df, remove_df) {
+
+  if (is.null(remove_df)) return(base_df)
+
+  is_duplicated <- apply(base_df, 1, function(pexe) {
+    is_matched <- apply(remove_df, 1, function(prem) {
+      all(pexe == prem)
+    })
+    return(any(is_matched))
+  })
+  return(base_df[!is_duplicated, ])
+
+}
+
+create_output_directory <- function(outdir, func_name, input_name = NULL) {
+
+  # abbreviate input file name
+  if (!is.null(input_name)) {
+    input_name <- basename(input_name)
+    seploc <- regexpr('__', input_name)
+    input_name_abb <- ifelse(
+      seploc == -1, 'Default',
+      substr(input_name, seploc + 2, nchar(input_name))
+    )
+  }
+
+  # determine output directory names
+  if (!is.null(input_name)) {
+    outdir_now <- paste(input_name_abb, collapse = '__')
+    outdir_now <- paste(outdir, func_name, outdir_now, sep = '__')
+  } else {
+    outdir_now <- paste(outdir, func_name, sep = '__')
+  }
+
+  # create directory
+  dir.create(outdir_now, recursive = TRUE, showWarnings = FALSE)
+  return(outdir_now)
+
+}
+
+convert_second_to_str <- function (totals) {
+
+  rests <- totals
+
+  second <- rests %% 60
+  rests <- rests - second
+  second <- round(second, 2)
+
+  minute <- rests %% 3600
+  rests <- rests - minute
+  minute <- minute / 60
+
+  hour <- rests %% 86400
+  rests <- rests - hour
+  hour <- hour / 3600
+
+  day <- rests / 86400
+
+  ans <- ''
+  ans <- paste0(ans, ifelse(day    == 0, '', paste0(day   , 'd ')))
+  ans <- paste0(ans, ifelse(hour   == 0, '', paste0(hour  , 'h ')))
+  ans <- paste0(ans, ifelse(minute == 0, '', paste0(minute, 'm ')))
+  ans <- paste0(ans, second, 's')
+
+  return(ans)
+
+}
+
+exe_new_proc_pattern <- function(
+  input_path_vec = NULL,
+  exe_func_list,
+  outdir,
+  exe_pattern = NULL,
+  remove_pattern = NULL
+) {
+
+  #' Executing sequential processing patterns which have not done before
+  #'
+  #' @param input_path_vec Lists of functions.
+  #' @param exe_func_list Lists of functions.
+  #' @param outdir Lists of functions.
+  #' @param remove_pattern A data frame indicating processing patterns
+  #' which should not be executed.
+  #'
+  #' @export
+
+  # create execution pattern of functions
+  if (is.null(exe_pattern)) {
+    exe_pattern <- data.frame(exe_func = names(exe_func_list))
+  }
+
+  # create execution pattern of input paths
+  for (path_name in names(input_path_vec)) {
+    # i_path <- 1
+
+    # fetch paths of input files
+    paths_i <- list.files(input_path_vec[path_name], full.names = TRUE)
+
+    # extend data frame to add paths
+    exe_pattern_now <- exe_pattern
+    for (iter in 1:length(paths_i)) {
+      if (iter == 1) next
+      exe_pattern <- rbind(exe_pattern, exe_pattern_now)
+    }
+
+    # add a column of input file paths
+    exe_pattern[[path_name]] <- rep(paths_i, each = nrow(exe_pattern_now))
+
+  }
+
+  # remove patterns which should not be executed
+  exe_pattern <- remove_dup_row(exe_pattern, remove_pattern)
+
+  # remove existing patterns
+  does_folder_exists <- length(list.files(outdir)) != 0
+  if (does_folder_exists) {
+    exist_pattern <- read.csv(paste0(outdir, '/ExecutedPatterns.csv'))
+    exist_pattern$folder <- NULL
+    exe_pattern <- remove_dup_row(exe_pattern, exist_pattern)
+  }
+
+  # cat execution patterns
+  cat('Detected', nrow(exe_pattern), 'patterns.\n')
+
+  # execution
+  exe_pattern_output <- exe_pattern
+  exe_pattern_output$folder <- NA
+  for (i_pat in 1:nrow(exe_pattern)) {
+    # i_pat <- 1
+
+    t1 <- proc.time()
+
+    # function to execute
+    exe_func_name_i <- exe_pattern$exe_func[[i_pat]]
+    exe_func_i <- exe_func_list[[exe_func_name_i]]
+
+    # file path to read
+    if (is.null(input_path_vec)) {
+      input_path_i <- NULL
+    } else {
+      input_path_i <- unlist(exe_pattern[i_pat, -1])
+      input_path_i <- paste0(input_path_i, '/')
+    }
+
+    # create output directory
+    outdir_i <- create_output_directory(outdir, exe_func_name_i, input_path_i)
+    outdir_i <- paste0(outdir_i, '/')
+    exe_pattern_output$folder[i_pat] <- outdir_i
+
+    # execution
+    cat('Pattern', i_pat, 'is running.\n')
+    cat('  Function:', exe_func_name_i, '\n')
+    if (is.null(input_path_vec)) {
+      exe_func_i(outdir_i)
+    } else {
+      for (j in 1:length(input_path_i)) {
+        cat('  ', names(input_path_vec)[j], ': ', input_path_i[j], '\n', sep = '')
+      }
+      exe_func_i(input_path_i, outdir_i)
+    }
+
+    # print time
+    proct <- convert_second_to_str((proc.time() - t1)[3])
+    cat('  Processing time: ', proct, '\n', sep = '')
+
+  }
+
+  # output execution patterns
+  write.csv(exe_pattern_output, paste0(dirname(outdir), '/ExecutedPatterns.csv'))
+  cat('Finished.\n')
+  return()
+
+}
+
+# 入力がない場合をチェック
+
