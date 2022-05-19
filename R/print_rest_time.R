3605b221e3cbda2813bef7ecda9065bb146b43a6
Yusuke Toda <yusuke0320.research@gmail.com>
2022-05-19 10:29:33 +0900

    Update print_rest_time.R

--- R/print_rest_time.R
+++ R/print_rest_time.R
@@ -2,10 +2,10 @@ print_rest_time <- function(iter, start_time, n_all, n_print) {
   
   if (iter %% n_print == 0) {
     
-    cat(as.character(Sys.time()))
+    cat('   ', as.character(Sys.time()))
     
     i_str <- format(iter, width = ceiling(log10(n_all + 1)))
-    cat('    iter:', i_str, '/', n_all)
+    cat('   iter:', i_str, '/', n_all)
     
     elapsed_time <- (proc.time() - start_time)[3]
     cat('   Elapsed:', convert_second_to_str(elapsed_time))
