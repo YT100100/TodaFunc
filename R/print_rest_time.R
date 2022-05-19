d4bb5e7a06a2eebcd9877c8cb7f6f09875ad3661
Yusuke Toda <yusuke0320.research@gmail.com>
2022-05-19 10:00:43 +0900

    Create print_rest_time.R
    for文内で、残りの解析時間を表示してくれる関数を作成

---
+++ R/print_rest_time.R
@@ -0,0 +1,16 @@
+print_rest_time <- function(iter, start_time, n_all, n_print) {
+  
+  if (iter %% n_print == 0) {
+    
+    i_str <- format(iter, width = ceiling(log10(n_all + 1)))
+    cat('    iter:', i_str, '/', n_all)
+    
+    elapsed_time <- (proc.time() - start_time)[3]
+    cat('   Elapsed:', convert_second_to_str(elapsed_time))
+    
+    pred_time <- elapsed_time / iter * (n_all - iter)
+    cat('   Predicted rest:', convert_second_to_str(pred_time), '\n')
+    
+  }
+  
+}
