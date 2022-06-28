ed90e0655366cdb9943b9d3172265d26dada125c
Yusuke Toda <yusuke0320.research@gmail.com>
2022-06-28 12:48:27 +0900

    Bug fix

--- R/print_rest_time.R
+++ R/print_rest_time.R
@@ -13,13 +13,13 @@ print_rest_time <- function(iter, n_iter, start_time, n_print) {
 
     cat('   ', as.character(Sys.time()))
 
-    i_str <- format(iter, width = ceiling(log10(n_all + 1)))
-    cat('   iter:', i_str, '/', n_all)
+    i_str <- format(iter, width = ceiling(log10(n_iter + 1)))
+    cat('   iter:', i_str, '/', n_iter)
 
     elapsed_time <- (proc.time() - start_time)[3]
     cat('   Elapsed:', convert_second_to_str(elapsed_time))
 
-    pred_time <- elapsed_time / iter * (n_all - iter)
+    pred_time <- elapsed_time / iter * (n_iter - iter)
     cat('   Rest:', convert_second_to_str(pred_time), '\n')
 
   }
