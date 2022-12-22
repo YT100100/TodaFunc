36d5b8bba7aef1f984844a8763b4ddb82e0ef628
Yusuke Toda <yusuke0320.research@gmail.com>
2022-12-22 15:55:00 +0900

    追加

--- R/ExpandedArray.R
+++ R/ExpandedArray.R
@@ -231,32 +231,3 @@ call_expanded_array <- function() {
 #   diminfo = list(paste0('SNP', 1:8), trait_info))
 # p_mat
 # p_mat$slice(, p_mat$diminfo[[2]]$year == 2021)
-
-
-
-#' Create an empty expanded array with diminfo only
-#'
-#' @param diminfo A list of information of the array.
-#'
-#' @importFrom R6 R6Class
-#' @export
-
-empty_expanded_array <- function(diminfo) {
-
-  dim <- sapply(diminfo, function(di) {
-
-    diminfo_length_i <- if ('ExpandedArray' %in% class(di)) {
-      di$length()
-    } else if ('data.frame' %in% class(di)) {
-      nrow(di)
-    } else {
-      length(di)
-    }
-
-  })
-
-  ExpArray <- call_expanded_array()
-  ans <- ExpArray$new(data = array(dim = dim), diminfo = diminfo)
-  return(ans)
-
-}
---
+++ R/empty_expanded_array.R
@@ -0,0 +1,26 @@
+#' Create an empty expanded array with diminfo only
+#'
+#' @param diminfo A list of information of the array.
+#'
+#' @importFrom R6 R6Class
+#' @export
+
+empty_expanded_array <- function(diminfo) {
+
+  dim <- sapply(diminfo, function(di) {
+
+    diminfo_length_i <- if ('ExpandedArray' %in% class(di)) {
+      di$length()
+    } else if ('data.frame' %in% class(di)) {
+      nrow(di)
+    } else {
+      length(di)
+    }
+
+  })
+
+  ExpArray <- call_expanded_array()
+  ans <- ExpArray$new(data = array(dim = dim), diminfo = diminfo)
+  return(ans)
+
+}
