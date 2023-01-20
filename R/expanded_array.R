a200fe233d7fcd6080f538aa4e81a27d3a11b855
Yusuke Toda <yusuke0320.research@gmail.com>
2023-01-20 11:27:53 +0900

    Create expanded_array.R
    A function to make ExpArray

---
+++ R/expanded_array.R
@@ -0,0 +1,16 @@
+#' Create an expanded array with data and diminfo
+#'
+#' @param data An array.
+#' @param diminfo A list of information of the array.
+#'
+#' @importFrom R6 R6Class
+#' @export
+#'
+
+empty_expanded_array <- function(data, diminfo) {
+
+  ExpArray <- call_expanded_array()
+  ans <- ExpArray$new(data = data, diminfo = diminfo)
+  return(ans)
+
+}
